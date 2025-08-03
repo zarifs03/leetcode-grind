b2cc5f349b6881eb06363a09d7a087c8267f7b65
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-08-03 16:40:36 +0800

    Added comments for time and space complexity -> 1. Two Sum

--- solutions/two_sum.py
+++ solutions/two_sum.py
@@ -8,4 +8,13 @@ class Solution:
             if diff in hashmap:
                 return [hashmap[diff], i]
             hashmap[num] = i
-                
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+        # O(n^2) solution
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
