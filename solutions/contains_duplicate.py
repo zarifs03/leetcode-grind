0c6b62974d1e4df902d033fd85c84a6757cd60f3
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-08-03 13:40:59 +0800

    first commit

---
+++ solutions/contains_duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+    
+# Time complexity: 0(n)
+# Space complexity: O(n)
