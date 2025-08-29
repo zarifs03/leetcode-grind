1eafcebdbc5b101bd4c9e2023627330bcbc4cbc9
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-08-29 17:47:43 +0800

    changed syntax

--- solutions/reduce_num_to_zero.py
+++ solutions/reduce_num_to_zero.py
@@ -2,12 +2,12 @@ class Solution:
     def numberOfSteps(self, num: int) -> int:
         steps = 0
 
-        while num != 0:
+        while num > 0:
             if num % 2 == 0:
-                num = num / 2
+                num /= 2
                 steps = steps + 1
             else:
-                num = num - 1
+                num -= 1
                 steps = steps + 1
         return steps
 
