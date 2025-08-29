701a22d0ae2e8d42fd603a82136e4dea84f78667
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-08-29 17:19:57 +0800

    1342. Number of steps to reduce a number to zero -- completed

---
+++ solutions/reduce_num_to_zero.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+
+        while num != 0:
+            if num % 2 == 0:
+                num = num / 2
+                steps = steps + 1
+            else:
+                num = num - 1
+                steps = steps + 1
+        return steps
+
+# Time complexity: O(log n)
+# Space complexity; O(1)
